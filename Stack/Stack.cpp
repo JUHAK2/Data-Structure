fa21e39e0aa88b6a25ff3de6a04df43e1d4959be
Guest <guest-devcz6@h28521.sgb.skir.kr>
2021-08-27 23:30:19 +0900

    const function added

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,53 +1,52 @@
 #include <iostream>
-#define MAXVALUE 20
 
 using namespace std;
 
 template <typename T>
-class Stack
+class Stack // "Stack" that stacks T
 {
 public:
-  T *values;
-  int topPoint;
-  int size;
-  Stack()
+  T* values;
+  int topIndex;
+  int maxSize;
+  Stack(const int size)
   {
-    size = MAXVALUE;
+    maxSize = size;
     values = new T[size];
-    topPoint = -1;
+    topIndex = -1;
   }
   ~Stack()
   {
     delete[] values;
   }
-  void push(T value)
+  void push(const T& value)
   {
     if(!isFull())
-      values[++topPoint] = value;
+      values[++topIndex] = value;
     else
       cout << "Stack is Full" << endl;
   }
   void pop()
   {
     if(!empty())
-      topPoint--;
+      topIndex--;
     else
       cout << "Stack is Empty";
   }
-  T top()
+  T top() const
   {
     if(!empty())
-      return values[topPoint];
+      return values[topIndex];
     else
       return NULL;
   }
-  bool empty()
+  bool empty() const
   {
-    return topPoint == -1;
+    return topIndex == -1;
   }
-  bool isFull()
+  bool isFull() const
   {
-    return topPoint+1 == size;
+    return topIndex + 1 == maxSize;
   }
 };
 
@@ -56,7 +55,7 @@ ostream& operator <<(ostream &out, Stack<T> &st)
 {
 	T *temp = st.values;
 	out << "┌───┐" <<endl;
-	for(int i=st.topPoint; i>=0; i--){
+	for(int i=st.topIndex; i >= 0; i--){
 	    out <<"  "<<temp[i]<< endl;
 	}
 	out << "└───┘" << endl;
@@ -65,7 +64,7 @@ ostream& operator <<(ostream &out, Stack<T> &st)
 
 int main()
 {
-	Stack<int> st;
+	Stack<int> st(10);
 	cout << "Stack push" << endl;
 	st.push(1);
 	cout << st << endl;
@@ -75,7 +74,7 @@ int main()
 	cout << "Stack push" << endl;
 	st.push(5);
 	cout << st <<endl;
-	cout << "Stack Top : " <<st.top() << endl << endl;
+	cout << "Stack Top : " << st.top() << endl << endl;
 	cout << "Stack pop" << endl;
 	st.pop();
 	cout << st << endl;
